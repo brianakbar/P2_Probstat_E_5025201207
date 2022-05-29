fa547af0a368581f463ff42a6c9254e7fd339906
brianakbar <94432967+brianakbar@users.noreply.github.com>
2022-05-29 19:24:00 +0700

    Add files via upload

--- E_5025201207.R
+++ E_5025201207.R
@@ -11,3 +11,16 @@ t.test(x,y,paired = TRUE)
 #-C-
 #Berdasarkan percobaan 1B, Alternative hypothesis bernilai true sehingga
 #h0 ditolak dan h1 diterima.
+
+
+#--Soal 2--
+
+#-A-
+#Tidak setuju
+
+#-B-
+zsum.test(mean.x = 23500,sigma.x = 3900,n.x = 100,
+          alternative = "less",mu = 20000)
+
+#-C-
+#p-value yang ditemukan adalah 1 yang berarti data yang dibandingkan sama
