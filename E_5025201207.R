a7edfcab6402e835477b4a891949ab02af58dfa5
brianakbar <94432967+brianakbar@users.noreply.github.com>
2022-05-29 19:42:29 +0700

    Add files via upload

--- E_5025201207.R
+++ E_5025201207.R
@@ -16,11 +16,11 @@ t.test(x,y,paired = TRUE)
 #--Soal 2--
 
 #-A-
-#Tidak setuju
+#Setuju
 
 #-B-
 zsum.test(mean.x = 23500,sigma.x = 3900,n.x = 100,
-          alternative = "less",mu = 20000)
+          alternative = "greater",mu = 20000)
 
 #-C-
-#p-value yang ditemukan adalah 1 yang berarti data yang dibandingkan sama
+#p-value yang ditemukan adalah 2.2e-16 yang berarti data yang dibandingkan tidak sama
