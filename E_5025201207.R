4a3b6e5873f97456f999c964e673cb78e2f0eb7e
brianakbar <94432967+brianakbar@users.noreply.github.com>
2022-05-29 22:09:23 +0700

    Add files via upload

--- E_5025201207.R
+++ E_5025201207.R
@@ -24,3 +24,46 @@ zsum.test(mean.x = 23500,sigma.x = 3900,n.x = 100,
 
 #-C-
 #p-value yang ditemukan adalah 2.2e-16 yang berarti data yang dibandingkan tidak sama
+
+
+#--Soal 3--
+
+#-A-
+#h0 adalah hipotesis rata-rata antara Bandung dan Bali sama.
+#h1 adalah hipotesis rata-rata antara Bandung dan Bali berbeda.
+
+#-B-
+tsum.test(mean.x=3.64, n.x = 19, s.x = 1.67, mean.y =2.79 , 
+          n.y = 27, s.y = 1.32, alternative = "two.side")
+
+#-D-
+qt(p=.05/2, df=2, lower.tail=FALSE)
+
+
+#--Soal 5--
+
+#-A-
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+
+GTL <- read_csv("GTL.csv")
+head(GTL)
+
+str(GTL)
+
+qplot(x = Temp, y = Light, geom = "point", data = GTL) +
+  facet_grid(.~Glass, labeller = label_both)
+
+#-B-
+GTL$Glass <- as.factor(GTL$Glass)
+GTL$Temp_Factor <- as.factor(GTL$Temp)
+str(GTL)
+
+#-C-
+data_summary <- group_by(GTL, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
